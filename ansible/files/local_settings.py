09d16a870e124cb90590ee2580ad56430ebeb0c7
Arjan Scherpenisse <arjan@scherpenisse.net>
2015-10-25 19:27:12 +0100

    Secure /hq/ with a password

--- ansible/files/local_settings.py
+++ ansible/files/local_settings.py
@@ -1,5 +1,7 @@
 # Where the GRIB2JSON utility finds itself
-GRIB2JSON_PATH = "/path/to/grib2json"
+GRIB2JSON_PATH = "/home/raduga/grib2json-0.8.0-SNAPSHOT/bin/grib2json"
+
+DEBUG = False
 
 # Required for uploading photos and sending push messages
 # See users.py for an explanation of these values
@@ -13,4 +15,4 @@ SERVER_NAME = 'vps40616.public.cloudvps.com'
 TEST_USER = "abc123abc123"
 
 # The password for the hq web interface
-HQ_PASSWORD = 'admin'
+HQ_PASSWORD = 'rdg1044'
--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import os
 from datetime import datetime
 
 # Dependencies: Flask + PIL or Pillow
-from flask import Flask, send_from_directory, redirect as redirect_flask, render_template
+from flask import Flask, send_from_directory, redirect as redirect_flask, render_template, request, Response
+from functools import wraps
+
 import pymongo
 
 # Local imports
@@ -15,6 +17,29 @@ app = Flask(__name__)
 
 client = pymongo.MongoClient()
 db = client.raduga
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == HQ_PASSWORD
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response('Please log in first', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 
 # Redirects should not be cached by the devices:
 def redirect(uri):
@@ -28,37 +53,47 @@ def redirect(uri):
     response.headers['Expires'] = '-1'
     return response
 
+
 # These static files should be served by the web server
 @app.route('/tiles/<path:filename>')
 def base_static(filename):
     return send_from_directory(os.path.join(app.root_path, 'raduga_tiles'), filename)
 
+
 @app.route('/')
 def index():
     return redirect('/hq/')
+
 
 @app.route("/latest/rainbows.json")
 def latest_rainbows():
     return redirect(get_latest_rainbows_url())
 
+
 @app.route("/latest/clouds.json")
 def latest_clouds():
     return redirect(get_latest_clouds_url())
+
 
 @app.route("/latest/rainbow_cities.json")
 def latest_rainbow_cities():
     return redirect(get_latest_rainbow_cities_url())
 
+
 @app.route("/hq/gfs/<string:slug>")
+@requires_auth
 def hq_slug(slug):
     return render_template("hq_slug.html", slug=slug)
 
+
 @app.route("/hq/")
+@requires_auth
 def hq():
-    logs = db.log.find().sort("$natural", pymongo.DESCENDING)
+    logs = db.log.find(limit=100).sort("$natural", pymongo.DESCENDING)
     forecasts = get_forecast_info()
     gfs = sorted(os.listdir(GFS_FOLDER))
     return render_template("hq.html", logs=logs, forecasts=forecasts, gfs=gfs)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
