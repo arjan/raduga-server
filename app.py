3e937bc66c781d1eb5f978dea62dd309a7b3f5eb
Arjan Scherpenisse <arjan@scherpenisse.net>
2015-10-26 12:36:11 +0100

    Add CORS

--- app.py
+++ app.py
@@ -3,9 +3,13 @@
 # Dependencies: Flask + PIL or Pillow
 from flask import Flask
 from flask.ext.cache import Cache
+from flask.ext.cors import CORS
+
 import pymongo
 
 app = Flask(__name__)
+
+cors = CORS(app)
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
--- main.py
+++ main.py
@@ -50,6 +50,5 @@ def get_closest_cities():
 
 hq.build()
 
-
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
