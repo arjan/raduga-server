96c7b68a42de63058e3052f7a97704e9b697827f
Arjan Scherpenisse <arjan@scherpenisse.net>
2019-06-25 10:44:30 +0200

    Fix fetch url

--- fetch.py
+++ fetch.py
@@ -18,9 +18,9 @@ def forecast_url(forecast_delta):
     d_rounded = d.replace(hour=(d.hour // 6)*6)
     forecast_hour = ((d.hour % 6 + forecast_delta) // 3) * 3
     slug = now.strftime("%Y%m%d%H")
-    slug_rounded = d_rounded.strftime("%Y%m%d%H")
+    slug_rounded = d_rounded.strftime("%Y%m%d%%2F%H")
 
-    url = "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl?file=gfs.t%sz.pgrb2full.0p50.f%03d&lev_entire_atmosphere_%%5C%%28considered_as_a_single_layer%%5C%%29=on&var_PWAT=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%%2Fgfs.%s" % (d_rounded.strftime("%H"), forecast_hour, slug_rounded)
+    url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl?file=gfs.t%sz.pgrb2full.0p50.f%03d&lev_entire_atmosphere_%%5C%%28considered_as_a_single_layer%%5C%%29=on&var_PWAT=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%%2Fgfs.%s" % (d_rounded.strftime("%H"), forecast_hour, slug_rounded)
 
     return slug, url
 
@@ -38,9 +38,9 @@ No dependencies outside the Python Standard Library
 def fetch_gfs():
     """
     Download the latest weather forecast from the Global Forecast System.
-    There are some 300 different tables in the GFS data, so we ask it to 
+    There are some 300 different tables in the GFS data, so we ask it to
     filtered for Precipitable Water (PWAT), the information that interests us.
-    
+
     The GFS data is produced every six hours. It contains information about
     the current weather situation and for 3, 6, 9, 12 etc. hours into
     the future. Because the GFS is not immediately available (in general
@@ -54,7 +54,7 @@ def fetch_gfs():
     for slug, url in urls:
         target_folder = os.path.join(GFS_FOLDER, slug)
         output_file = os.path.join(target_folder, "GFS_half_degree.%s.pwat.grib" % slug)
-        
+
         if os.path.exists(output_file):
             # There is no need to continue looking, as we have this file already,
             # and apparently it is the most recent forecast.
